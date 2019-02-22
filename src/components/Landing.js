e7c3b84d51e4dcac8b00d4baee8b8a709520397a
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-22 11:04:30 -0500

    More scaffolding, and adding images to landing page.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import logo from '../assets/waffly-logo.png';
+import landingArt from '../assets/waffly-landing-art.png';
 
 class Landing extends React.Component {
   render() {
     return (
-      <div>
-        Welcome to Waffly!
+      <div id="landing" className="container">
+        <div className="row">
+          <div className="col-12 col-sm-6">
+            <h1 className="display-3">
+              <img
+                src={logo}
+                style={{height: '40px'}}
+              />
+              Waffly
+            </h1>
+            <p>
+              creating simple projects
+              <br/>
+              that combine
+              <br/>
+              beauty and function.
+            </p>
+          </div>
+          <div className="col-12 col-sm-6">
+            <img
+              src={landingArt}
+              style={{height: '350px'}}
+            />
+          </div>
+        </div>
       </div>
     );
   }
