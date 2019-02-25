a8262b1d6060d822162596c6718ffc3d0c039df2
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-25 11:54:46 -0500

    WFLY-1: Adding ClosetrFeature page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Landing from './Landing';
+import ClosetrFeature from './ClosetrFeature';
 import "../styles/styles.scss";
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <Landing/>
+        <ClosetrFeature/>
       </div>
     );
   }
---
+++ src/components/ClosetrFeature.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ClosetrFeature extends React.Component {
+  render() {
+    return (
+      <div id="closetr-feature" className="container">
+
+      </div>
+    );
+  }
+}
+
+export default ClosetrFeature;
