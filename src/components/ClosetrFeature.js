2bb31496dac2f17eff8323500a87738e816260ba
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-25 13:33:22 -0500

    WFLY-1: Styling closetr feature into landing page.

--- src/components/ClosetrFeature.js
+++ src/components/ClosetrFeature.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import closetrFeatureImg from '../assets/closetr-feature-img.png';
 
 class ClosetrFeature extends React.Component {
   render() {
     return (
-      <div id="closetr-feature" className="container">
-
+      <div id="closetr-feature">
+        <div className="page container">
+          <img
+            className="feature-img"
+            src={closetrFeatureImg}
+          />
+        </div>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,42 +6,44 @@ import arrowDown from '../assets/arrow-down.png';
 class Landing extends React.Component {
   render() {
     return (
-      <div id="landing" className="container">
-        <div className="row">
-          <div className="col-12 col-md-6">
-            <h1>
-              <img
-                src={logo}
-                className="waffly-logo"
-              />
-              <br/>
-              WAFFLY
-            </h1>
-            <span className="subtitle">
-              creating simple projects
-              <br/>
-              that combine
-              <br/>
-              beauty and function.
-            </span>
-          </div>
-          <div className="col-12 col-md-6">
-            <img
-              src={landingArt}
-              style={{width: '100%'}}
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <div className="expand-caption">
-              <span className="subtitle-sm">
-                What we've been working on
+      <div id="landing">
+        <div className="page container">
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <h1>
+                <img
+                  src={logo}
+                  className="waffly-logo"
+                />
+                <br/>
+                WAFFLY
+              </h1>
+              <span className="subtitle">
+                creating simple projects
+                <br/>
+                that combine
+                <br/>
+                beauty and function.
               </span>
+            </div>
+            <div className="col-12 col-md-6">
               <img
-                src={arrowDown}
-                className="icon-arrow-down"
+                src={landingArt}
+                style={{width: '100%'}}
               />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="expand-caption">
+                <span className="subtitle-sm">
+                  What we've been working on
+                </span>
+                <img
+                  src={arrowDown}
+                  className="icon-arrow-down"
+                />
+              </div>
             </div>
           </div>
         </div>
