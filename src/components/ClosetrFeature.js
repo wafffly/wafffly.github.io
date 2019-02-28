a0509649639098df098edf91d5e49f5d873a9c9f
Fides Linga <fides.linga@mail.utoronto.ca>
2019-02-28 14:12:13 -0500

    WFLY-1: Fixing typo and padding issue at bottom of page.

--- src/components/ClosetrFeature.js
+++ src/components/ClosetrFeature.js
@@ -15,19 +15,18 @@ class ClosetrFeature extends React.Component {
               closetr
             </div>
             <div className="caption">
-              Closetr is a closet tracker that helps you ahve a better
+              Closetr is a closet tracker that helps you have a better
               sense of your style, and spending. We want to make it
               easier to know what to wear next, and to give you insight
               on which clothes work (and don't work) for you.
             </div>
-            <button className="btn demo">
+            <button className="btn demo" style={{marginBottom: '50px'}}>
               <a href="http://fideslinga.com/work/closetr">
                 VIEW PROJECT
               </a>
             </button>
           </div>
         </div>
-        <div id="closetr-feature-bg"></div>
       </div>
     );
   }
