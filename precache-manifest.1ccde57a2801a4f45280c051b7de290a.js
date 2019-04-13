e9629d0036fc692ceb12b5ba493eba707034566c
desslinga <fides.linga@mail.utoronto.ca>
2019-04-12 22:14:42 -0400

    Updates

---
+++ precache-manifest.1ccde57a2801a4f45280c051b7de290a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "9af5b0ee5894d005f218ff5d80d155d7",
+    "url": "/static/media/closetr_mockups_web-log-outfit-screen.9af5b0ee.png"
+  },
+  {
+    "revision": "c1e4b8f428993b369c06",
+    "url": "/static/css/main.64c70b12.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9cdef60bb035d70483b030b110263f58",
+    "url": "/static/media/waffly-landing-art.9cdef60b.png"
+  },
+  {
+    "revision": "83536a2ceded22138799",
+    "url": "/static/js/2.c3f63538.chunk.js"
+  },
+  {
+    "revision": "4e51b892317e254583338b1be432242f",
+    "url": "/static/media/closetr-feature-img.4e51b892.png"
+  },
+  {
+    "revision": "f390073d7ea5332d98338530aadf5248",
+    "url": "/static/media/sparknodes-feature-img.f390073d.png"
+  },
+  {
+    "revision": "84e8580ea05eb3b0d610df42bde868e0",
+    "url": "/static/media/arrow-down.84e8580e.svg"
+  },
+  {
+    "revision": "f4218dc51e24338f9fefb88b1846bafa",
+    "url": "/static/media/closetr_mockups_web-add-clothing-screen.f4218dc5.png"
+  },
+  {
+    "revision": "e3c1d221a901c5ad9526af606e6d5467",
+    "url": "/static/media/closetr_mockups_web-closet-search-screen.e3c1d221.png"
+  },
+  {
+    "revision": "22cbe4d197a0b96246cf766efbd1a2c6",
+    "url": "/static/media/closetr_mockups_web-dashboard.22cbe4d1.png"
+  },
+  {
+    "revision": "c1e4b8f428993b369c06",
+    "url": "/static/js/main.1530fd58.chunk.js"
+  },
+  {
+    "revision": "80ecaa66ea56bbff17613c8f6105b3c0",
+    "url": "/static/media/closetr_mockups_web-log-outfit-search-screen.80ecaa66.png"
+  },
+  {
+    "revision": "005ec49b7b13bfc4544c9e0d5bb7e393",
+    "url": "/static/media/sparknodes-design-1.005ec49b.png"
+  },
+  {
+    "revision": "8afb1dad610f6cfaf24ec0d471a1dbb3",
+    "url": "/static/media/sparknodes-design-2.8afb1dad.png"
+  },
+  {
+    "revision": "f841ca0fdf57e72d5bf253ae4682490c",
+    "url": "/static/media/sparknodes-design-3.f841ca0f.png"
+  },
+  {
+    "revision": "c9595f4804ec9ca85fa46053ee0bd1c5",
+    "url": "/static/media/sparknodes-screen-1.c9595f48.png"
+  },
+  {
+    "revision": "8ccef6fe0b73873dda60923738c86782",
+    "url": "/static/media/sparknodes-screen-2.8ccef6fe.png"
+  },
+  {
+    "revision": "05c3e2df5d9a86c87e5289f3fafb7b36",
+    "url": "/static/media/sparknodes-screen-3.05c3e2df.png"
+  },
+  {
+    "revision": "9de304cdfaf0d4018ee56d534d81fbac",
+    "url": "/static/media/sparknodes-screen-4.9de304cd.png"
+  },
+  {
+    "revision": "96787c7740eb7a421961930e9f533e53",
+    "url": "/static/media/landing-header.96787c77.svg"
+  },
+  {
+    "revision": "83536a2ceded22138799",
+    "url": "/static/css/2.de20c360.chunk.css"
+  },
+  {
+    "revision": "8fc5dcfda412be09274adbdd2cf07242",
+    "url": "/index.html"
+  }
+];
